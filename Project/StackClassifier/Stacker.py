939d1f0d0243627f14ea3c1e4335d8e4ce456388
Viktor Thorén <viktor.thoren@physics.uu.se>
2021-03-03 14:14:55 +0100

    Streamlined data reading feature of stacker. Combined the three previous functions into one that covers all functionality

--- Project/StackClassifier/Stacker.py
+++ Project/StackClassifier/Stacker.py
@@ -1,6 +1,6 @@
 import uproot
 import pandas as pd
-import ROOT
+#import ROOT
 import numpy
 import tensorflow as tf
 from sklearn.ensemble import RandomForestClassifier
@@ -9,8 +9,8 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from array import array
 
-from rootInterfaces import predOutputRoot
-from rootInterfaces import dataLoader
+#from rootInterfaces import predOutputRoot
+#from rootInterfaces import dataLoader
 
 
 BDTBGFile = "BDTBGPreds.csv"
@@ -20,93 +20,42 @@ MLPSigFile = "MLPSigPreds.csv"
 kNNBGFile = "kNNBGPreds.csv"
 kNNSigFile = "kNNSigPreds.csv"
 
-bgFileList = ["BDTBGPreds.csv", "MLPBGPreds.csv", "kNNBGPreds.csv"]
-sigFileList = ["BDTSigPreds.csv", "MLPSigPreds.csv", "kNNSigPreds.csv"]
+fileList = ["BDTPreds.csv", "MLPPreds.csv", "kNNPreds.csv"]
 
 #def Stacker(BDTBGFile, BDTSigFile, MLPBGFile, MLPSigFile, kNNBGFile, kNNSigFile):
-def Stacker(bgFileList, sigFileList):
+def Stacker(fileList, includeSignal=True, includeBG=True, outName="StackedPreds.csv"):
      """ Builds combined dataframe from output of several models.
 
      Parameters
      ----------
-     bgFileList : list of str
-        List of names of files containing background input data.
-     sigFileList : list of str
-        List of names of files containing signal input data.
-     BDTBGFile : str
-        Name of file containing BDT predictions for background data.
-     BDTSig : str
-        Name of file containing BDT predictions for signal data.
-     MLPBGFile : str
-        Name of file containing MLP predictions for background data.
-     MLPSig : str
-        Name of file containing MLP predictions for signal data.
-     kNNBGFile : str
-        Name of file containing kNN predictions for background data.
-     kNNSig : str
-        Name of file containing kNN predictions for signal data.
+     fileList : list of str
+        List of names of files containing input data.
+     includeSignal : bool
+        Controls whether signal events are included in output dataframe. Default is true/yes.
+     includeBG : bool
+        Controls whether bg events are included in output dataframe. Default is true/yes.
+     outName : str
+        Name of output file. Default is StackedPreds.csv
 
 
 
      """
-     BDT_BG_DF = pd.read_csv(BDTBGFile)
-     BDT_Sig_DF = pd.read_csv(BDTSigFile)
-     MLP_BG_DF = pd.read_csv(MLPBGFile)
-     MLP_Sig_DF = pd.read_csv(MLPSigFile)
-     kNN_BG_DF = pd.read_csv(kNNBGFile)
-     kNN_Sig_DF = pd.read_csv(kNNSigFile)
-     
-     BDT_DF = pd.concat([BDT_BG_DF, BDT_Sig_DF]).reset_index(drop=True)
-     MLP_DF = pd.concat([MLP_BG_DF, MLP_Sig_DF]).reset_index(drop=True)
-     kNN_DF = pd.concat([kNN_BG_DF, kNN_Sig_DF]).reset_index(drop=True)
-     
-     comb_DF = pd.concat([BDT_DF,MLP_DF['MLPPred'], kNN_DF['kNNPred']], axis=1)
-     
+     comb_DF = pd.DataFrame()
+     for f in fileList:
+        temp_DF = pd.read_csv(f)
+        comb_DF = pd.concat([comb_DF, temp_DF], axis=1)
+
+     comb_DF = comb_DF.loc[:,~comb_DF.columns.duplicated()]
+     if (not includeSignal): 
+         comb_DF = comb_DF[comb_DF.label != 1].reset_index()
+     if (not includeBG): 
+         comb_DF = comb_DF[comb_DF.label != 0].reset_index()
+
      print(comb_DF)
      
      comb_DF.to_csv("StackedPreds.csv",index=False)
    
 
-def SigStacker(BDTSigFile, MLPSigFile):
-     """ Builds combined dataframe of only signal predictions
-     
-     Parameters
-     ----------
-     BDTSig : str
-        Name of file containing BDT predictions for signal data.
-     MLPSig : str
-        Name of file containing MLP predictions for signal data.
-
-    
-     """
-     BDT_Sig_DF = pd.read_csv(BDTSigFile)
-     MLP_Sig_DF = pd.read_csv(MLPSigFile)
-     
-     #comb_DF = pd.concat([BDT_Sig_DF,MLP_Sig_DF['MLPPred']], axis=1)
-     comb_DF = pd.concat([BDT_DF,MLP_DF['MLPPred'], kNN_DF['kNNPred']], axis=1)
-     
-     comb_DF.to_csv("StackedSigPreds.csv",index=False)
-
-def BGStacker(BDTBGFile, MLPBGFile):
-     """ Builds combined dataframe of only background predictions.
-     
-     Parameters
-     ----------
-     BDTSig : str
-        Name of file containing BDT predictions for signal data.
-     MLPSig : str
-        Name of file containing MLP predictions for signal data.
-
-     """
-
-     BDT_BG_DF = pd.read_csv(BDTBGFile)
-     MLP_BG_DF = pd.read_csv(MLPBGFile)
-     
-     comb_DF = pd.concat([BDT_BG_DF,MLP_BG_DF['MLPPred']], axis=1)
-     
-     comb_DF.to_csv("StackedBGPreds.csv",index=False)
 
 
-#Stacker(BDTBGFile, BDTSigFile,MLPBGFile,MLPSigFile, kNNBGFile, kNNSigFile)
-SigStacker(BDTSigFile, MLPSigFile)
-#BGStacker(BDTBGFile, MLPBGFile)
+Stacker(fileList, includeSignal = False)
