37eee559a9198ae7e4275b08ff3fca6fdbc1f966
Viktor Thorén <viktor.thoren@physics.uu.se>
2021-03-04 11:40:28 +0100

    Changed from absolute to relative path to source code in sphinx config file

--- Project/StackClassifier/docs/source/conf.py
+++ Project/StackClassifier/docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/home/viktor/Documents/AdvPython/Project/StackClassifier'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
