f8a3e28c20da27e2d2df28cd713a4bbb9ae34aa5
Viktor Thorén <viktor.thoren@physics.uu.se>
2021-03-04 12:07:47 +0100

    Add additional path to sphinx conf file.

--- Project/StackClassifier/docs/source/conf.py
+++ Project/StackClassifier/docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.append(os.path.abspath('../../'))
+sys.path.append(os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
